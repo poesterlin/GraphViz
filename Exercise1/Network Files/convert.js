d78a08e094f0b80d11a1832243222b45cc7239f4
Philip Oesterlin <philip.oesterlin+github@gmail.com>
2019-10-27 00:28:25 +0200

    exercise 1

---
+++ Exercise1/Network Files/convert.js
@@ -0,0 +1,57 @@
+
+const fs = require('fs');
+var parseString = require('xml2js').parseString;
+
+// load xml
+var xml = fs.readFileSync('string_interactions_prkaca.xml');
+
+
+// parse xml
+parseString(xml, (_, result) => {
+    const nodes = buildNodes(result.set.interactorList[0].interactor);
+    const edges = buildEdges(result.set.interactionList[0].interaction)
+
+    fs.writeFileSync('string_interactions_prkaca.graphml', stringify(nodes, edges));
+    console.log("file was converted")
+});
+
+// pull node info from interactor list
+function buildNodes(nodes) {
+    return nodes.map(n => {
+        return { label: n.names[0].shortLabel[0], id: n.$.id };
+    });
+}
+
+// pull edge info from interaction list
+function buildEdges(edges) {
+    return edges.map((e) => {
+        return {
+            from: e.participantList[0].participant[0].interactorRef[0],
+            to: e.participantList[0].participant[1].interactorRef[0]
+        }
+    });
+}
+
+
+// convert to graphml format
+function stringify(nodes, edges) {
+    const fileHeader = `<graphml>
+<key attr.name="name" attr.type="string" for="node" id="name"/>
+<graph edgedefault="directed" id="">`
+
+    const fileFooter = `</graph></graphml>`
+
+    let output = fileHeader;
+
+    output += nodes.map((n) => ` 
+    <node id="${n.id}">
+        <data key="name">${n.label}</data>
+    </node>`
+    ).join("");
+
+    output += edges.map((e) => `<edge source="${e.from}" target="${e.to}"/>`).join("");
+
+    output += fileFooter;
+
+    return output;
+}
